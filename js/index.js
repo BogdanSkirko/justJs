caf79443b8b308339dda8ac954aa771a565e52b9
Bogdanskirko <Bogdanskirko@gmail.com>
2022-06-24 19:10:45 +0300

    completed CoddingChallenge

--- js/index.js
+++ js/index.js
@@ -166,5 +166,80 @@ const checkWinner = (avgDolphins1, avgKoalas1) =>
 
 checkWinner(avgDolphins1, avgKoalas1);
 
-let numer = [3, 3, 3];
-console.log(numer.length - 1);
+// Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) �
+// GOOD LUCK �
+
+const calcTip = (anyBill) =>
+  anyBill > 300 || anyBill < 50
+    ? anyBill * 1.2 - anyBill
+    : anyBill * 1.15 - anyBill;
+
+const arrayBills = [125, 555, 44];
+const arrayTips = [
+  calcTip(arrayBills[0]),
+  calcTip(arrayBills[1]),
+  calcTip(arrayBills[2]),
+];
+calcTip(arrayBills);
+console.log(arrayTips);
+const arrayBillsAndTips = [
+  arrayBills[0] + arrayTips[0],
+  arrayBills[1] + arrayTips[1],
+  arrayBills[2] + arrayTips[2],
+];
+console.log(arrayBillsAndTips);
+
+// Coding Challenge #3
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+// implement the calculations! Remember: BMI = mass / height ** 2 = mass
+// / (height * height) (mass in kg and height in meter)
+// Your tasks:
+// 1. For each of them, create an object with properties for their full name, mass, and
+// height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+// method on both objects). Store the BMI value to a property, and also return it
+// from the method
+// 3. Log to the console who has the higher BMI, together with the full name and the
+// respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+// Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+// tall.
+// GOOD LUCK �
+const markObj = {
+  fullname: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBmi: () => markObj.mass / markObj.height ** 2,
+};
+const johnObj = {
+  fullname: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBmi: () => johnObj.mass / johnObj.height ** 2,
+};
+console.log(
+  markObj.calcBmi() > johnObj.calcBmi()
+    ? `${
+        markObj.fullname
+      } ${markObj.calcBmi()} 'is higher than' ${johnObj.calcBmi()} 'BMI"s' `
+    : `${johnObj.fullname} ${johnObj.calcBmi()} 'BMI"s  is hither than' ${
+        markObj.fullname
+      } ${markObj.calcBmi()} BMI"s  `
+);
