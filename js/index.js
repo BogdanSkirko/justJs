019c51235004268f2d79abe803550a747fadf044
Bogdanskirko <Bogdanskirko@gmail.com>
2022-06-24 12:02:18 +0300

    coddinchallenge1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
-console.log(32121);
-console.log(32121);
-console.log(32121);
-console.log(32121);
-console.log(32121);
-console.log(32121);
-console.log(32121);
-console.log(32121);
+const marksMass = 78;
+const jonhsMass = 92;
+const marksHeight = 1.69;
+const jonhsHeight = 1.95;
+
+const marksBmi = (marksMass / marksHeight) * 2;
+const jonhssBmi = (jonhsMass / jonhsHeight) * 2;
+const markHigherBmi = marksBmi > jonhssBmi;
+console.log(markHigherBmi);
+
+const marksMass1 = 95;
+const jonhsMass1 = 85;
+const marksHeight1 = 1.88;
+const jonhsHeight1 = 1.76;
+
+const marksBmi1 = (marksMass1 / marksHeight1) * 2;
+const jonhssBmi1 = (jonhsMass1 / jonhsHeight1) * 2;
+const markHigherBmi1 = marksBmi1 > jonhssBmi1;
+console.log(markHigherBmi1);
