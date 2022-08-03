fa9e3ac8df6c05f5741ab765114d357d63c38160
BogdanSkirko <BogdanSkirko@gmail.com>
2022-08-03 18:29:50 +0300

    end CG OOP

--- js/OOP/script.js
+++ js/OOP/script.js
@@ -16,20 +16,20 @@
 // § Data car 2: 'Mercedes' going at 95 km/h
 // GOOD LUCK �
 
-const Car = function (make, speed) {
-  (this.make = make), (this.speed = speed);
-};
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`${this.make} going at ${this.speed} km/h`);
-};
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(`${this.make} going at ${this.speed} km/h`);
-};
+// const Car = function (make, speed) {
+//   (this.make = make), (this.speed = speed);
+// };
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`${this.make} going at ${this.speed} km/h`);
+// };
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} going at ${this.speed} km/h`);
+// };
 
-const bmw = new Car('BMW', 120);
-const mercedes = new Car('MERCEDES', 120);
+// const bmw = new Car('BMW', 120);
+// const mercedes = new Car('MERCEDES', 120);
 // bmw.accelerate();
 // bmw.brake();
 // bmw.accelerate();
@@ -63,6 +63,7 @@ class CarCl {
   brake = function () {
     this.speed -= 5;
     console.log(`${this.make} going at ${this.speed} km/h`);
+    return this;
   };
 
   get speedUs() {
@@ -72,12 +73,12 @@ class CarCl {
     this.speed = speed * 1.6;
   }
 }
-const ford = new CarCl('Ford', 120);
-console.log(ford.speedUs);
-ford.speedUs = 120;
-console.log(ford);
-ford.accelerate();
-ford.brake();
+// const ford = new CarCl('Ford', 120);
+// console.log(ford.speedUs);
+// ford.speedUs = 120;
+// console.log(ford);
+// ford.accelerate();
+// ford.brake();
 
 // Coding Challenge #3
 // Your tasks:
@@ -96,23 +97,63 @@ ford.brake();
 // § Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
 // GOOD LUCK
 
-const EV = function (make, speed, charge) {
-  Car.call(this, make, speed);
-  this.charge = charge;
-};
-EV.prototype = Object.create(Car.prototype);
-EV.prototype.chargeBattery = function (chargeTo) {
-  this.charge = chargeTo;
-};
-EV.prototype.accelerate = function () {
-  this.speed += 20;
-  this.charge--;
-  console.log(
-    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}`
-  );
-};
-const tesla = new EV('Tesla', 120, 23);
-console.log(tesla);
-tesla.accelerate();
-tesla.accelerate();
-tesla.brake();
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+// EV.prototype = Object.create(Car.prototype);
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge--;
+//   console.log(
+//     `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}`
+//   );
+// };
+// const tesla = new EV('Tesla', 120, 23);
+// console.log(tesla);
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.brake();
+
+// Coding Challenge #4
+// Your tasks:
+// 1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl'
+// child class of the 'CarCl' class
+// 2. Make the 'charge' property private
+// 3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+// methods of this class, and also update the 'brake' method in the 'CarCl'
+// class. Then experiment with chaining!
+// Test data:
+// § Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+// GOOD LUCK �
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+rivian.accelerate().accelerate().accelerate().brake();
